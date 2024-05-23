598cf18e5a324c6d3bf996ab4f85733a3fb652a3
jawhlan <enhpurevjawhlan@gmail.com>
2024-05-23 21:55:19 +0800

    Routes and tailwind css added

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-700">Hello I am Javkhaa</h1>
+    </div>
+  );
+}
+
+export default Home;
