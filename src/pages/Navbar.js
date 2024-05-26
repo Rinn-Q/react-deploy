c48344ff651ca4315a40da1c9dba38a5eb23fcd9
jawhlan <enhpurevjawhlan@gmail.com>
2024-05-26 23:07:51 +0800

    toggle zassan

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,4 +1,7 @@
 import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from "@fortawesome/free-solid-svg-icons/faMoon";
 
 function Navbar() {
 
@@ -11,21 +14,24 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div style={{display: 'flex'}}>
-            <img src={`${process.env.PUBLIC_URL}/img/avatar.jpg`} alt="Avatar" className="avatar"/>
-                <a className="navbar-brand" href="/">Portfolio</a>
+                <img src={`${process.env.PUBLIC_URL}/img/avatar.jpg`} alt="Avatar" className="avatar"/>
             </div>
             <ul>
-                <a href="/"><li>Home</li></a>
                 <a href="/"><li>About</li></a>
-                <a href="/"><li>Services</li></a>
+                <a href="/"><li>Portfolio</li></a>
+                <a href="/"><li>Contact</li></a>
             </ul>
-            <button onClick={toggleTheme} id="colorToggler" className="flex items-center">
+            <button onClick={toggleTheme} id="colorToggler" className="flex items-center ">
                 
                     {isDarkMode ? 
                     (
-                        <div className='bg-gray-700 w-4 h-4 rounded-full ml-2' />
+                        <div className='bg-gray-700 w-5 h-5 rounded-full ml-3 flex items-center' >
+                            <FontAwesomeIcon icon={faMoon} size="xs" className="ml-1.5 text-white"/>
+                        </div>
                     ) : (
-                        <div className='bg-yellow-300 w-4 h-4 rounded-full ml-0' />
+                        <div className='bg-yellow-300 w-5 h-5 rounded-full ml-0 flex items-center'>
+                            <FontAwesomeIcon icon={faSun} size="xs" className="ml-1 text-white"/>
+                        </div>
                     )}
                 
             </button>
