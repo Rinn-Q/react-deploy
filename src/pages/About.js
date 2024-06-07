b358d58d52c5c58c097a962ced76f08a808a1423
jawhlan <enhpurevjawhlan@gmail.com>
2024-06-07 12:47:57 +0800

    About end

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,8 @@ function About({isDarkMode}) {
   return (
     <div className={`${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white text-black'} container p-5`}>
       <h1 className='text-2xl font-mono'>About</h1>
-      <p className='text-xl font-mono'>some text here ...</p>
+      <p className='text-l font-mono'>My name is Javhlan. 2nd year student of Software Engineering, School of Information and
+        Communication Technology, Mongolian University of Science and Technology. Currently working as an intern at onLime network LLC.</p>
       <Skills isDarkMode={isDarkMode}/>
     </div>
   );
